ef32252c69874e95df7d4c5264c63889d11bbda8
lichanghao <542103276@qq.com>
2025-03-05 22:25:38 -0500

    minor change

--- src/AGENT/fix_nve_body_agent.cpp
+++ src/AGENT/fix_nve_body_agent.cpp
@@ -63,6 +63,7 @@ FixNVEBodyAgent::FixNVEBodyAgent(LAMMPS *lmp, int narg, char **arg) :
   atom->add_callback(Atom::BORDER);
   
   // initiate the image flag for all atoms as 0, because somehow the original body package did not do it
+  int nlocal = atom->nlocal;
   for (int i = 0; i < nlocal; i++) atom->image[i] = 0;
 
   // find maximum id across all processors
--- src/fix_nve_body_agent.cpp
+++ src/fix_nve_body_agent.cpp
@@ -63,7 +63,8 @@ FixNVEBodyAgent::FixNVEBodyAgent(LAMMPS *lmp, int narg, char **arg) :
   atom->add_callback(Atom::BORDER);
   
   // initiate the image flag for all atoms as 0, because somehow the original body package did not do it
-  // for (int i = 0; i < nlocal; i++) atom->image[i] = 0;
+  int nlocal = atom->nlocal;
+  for (int i = 0; i < nlocal; i++) atom->image[i] = 0;
 
   // find maximum id across all processors
   maxtag_all = 1E5;
