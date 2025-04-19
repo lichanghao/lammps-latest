1ae9aa8dbc56da020908676ae86d29c434ec5f6c
lichanghao <542103276@qq.com>
2025-04-19 18:27:52 -0400

    deleted hard-core cell-surface interaction

--- src/fix_nve_body_agent.cpp
+++ src/fix_nve_body_agent.cpp
@@ -573,8 +573,8 @@ void FixNVEBodyAgent::apply_cell_surface_force(int ibody, double *omega, double 
   if (center_coords[2] - ori_vec[2]*L/2.0 < R - hc_threshold) {
     vector<double> cell_surface_repulsion_force = cellSurfaceRepulsionForce(center_coords, ori_vec, L/2.0, R - hc_threshold, E_EPS * hc_scaling);
     for (int j = 0; j < 3; j++) {
-      f[ibody][j] += cell_surface_repulsion_force[j];
-      torque[ibody][j] += cell_surface_repulsion_force[j+3];
+      // f[ibody][j] += cell_surface_repulsion_force[j];
+      // torque[ibody][j] += cell_surface_repulsion_force[j+3];
     }
   }
 }
